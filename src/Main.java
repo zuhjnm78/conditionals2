28e3f7c205f4383241cc6287a2c58156ee9d0362
oleghenkel <oleg.henkel75@gmail.com>
2023-04-20 00:25:17 +0200

    hw6 improved

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-        task4();
-        task5();
+            task1();
+            task2();
+            task3();
+            task4();
+            task5();
 
 
-    }
+        }
+
 
     public static void task1() {
 
@@ -31,17 +34,24 @@ public class Main {
 
         // Пишем код для задачи 2
 
-        char clientOS1 = 1;
+        int clientOS = 0;
         int clientDeviceYear = 2015;
-        if (clientOS1 == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS1 == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientOS1 == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке.");
+        int currentYear = 2023;
+        if (clientDeviceYear < currentYear) {
+            if (clientOS == 0 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS.");
+            } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            } else if (clientOS == 1 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android.");
+            } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            } else if (clientOS != 0 && clientOS != 1) {
+                System.out.println("Ошибка: неверный тип операционной системы.");
+            } else {
+                System.out.println("Устройство не нуждается в обновлении.");
+            }
         }
-        else if (clientOS1 == 1 && clientDeviceYear < 2015)
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
     }
 
     public static void task3() {
@@ -50,9 +60,9 @@ public class Main {
 
         // Пишем код для задачи 3
 
-        int year = 2024;
+        int year = 2021;
         int d = year % 4;
-        if (d != 0) {
+        if (d != 0 || year % 400 != 0 && year % 100 == 0) {
             System.out.println(year + " год не является високосным.");
         }
         else
